6eb9e913bc019589cf7614e25803d63a2190dd91
Tim Robilko <timur.robilko@gmail.com>
2023-11-26 13:26:31 +0300

    Основной функционал

---
+++ Sem3/LW_add5/FibonacciHeap/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace FibonacciHeap;
+
+class Program
+{
+    static void Main()
+    {
+        FibonacciHeap<object, int> heap = new(0);
+
+        FibonacciHeapNode<object, int> node1 = new("wow1", 2);
+        FibonacciHeapNode<object, int> node2 = new("wow2", 2);
+        FibonacciHeapNode<object, int> node3 = new("wow3", 3);
+
+        heap.Insert(node1);
+        heap.Insert(node2);
+        heap.Insert(node3);
+
+        heap.Delete(node2);       
+    }
+}
